3a4e928af03be25071838a082d6a63ca20f1ab76
tstelter <tobias.stelter@outlook.com>
2022-07-07 19:15:31 +0200

    added more text preprocessing

--- Main/Main.py
+++ Main/Main.py
@@ -1,8 +1,6 @@
-# from Twitter.TweetStreamListener import TweetStreamListener
+from Twitter.TweetStreamListener import TweetStreamListener
 from TextPreprocessing import TextPreprocess
-from TextPreprocessing.Helpers.StopWords import StopWords
 
 
 class Main:
-    text = 'Test :)'
-    print(TextPreprocess.preprocess(text))
+    pass
--- TextPreprocessing/TextPreprocess.py
+++ TextPreprocessing/TextPreprocess.py
@@ -3,9 +3,12 @@ import Config.text
 import contractions
 import nltk
 
+from typing import List, Union
+from nltk import word_tokenize
 from nltk.stem import SnowballStemmer
 from nltk.stem import WordNetLemmatizer
 from string import punctuation
+from spellchecker import SpellChecker
 from TextPreprocessing.Helpers import Smileys
 from TextPreprocessing.Helpers import Emojis
 from TextPreprocessing.Helpers.StopWords import StopWords
@@ -21,6 +24,7 @@ def preprocess(text):
     text = replace_emojis(text)
     text = remove_leetspeak(text)
     text = remove_numbers(text)
+    text = check_spelling(text)
     text = replace_contractions(text)
     text = remove_punct(text)
     text = to_lower(text)
@@ -123,3 +127,22 @@ def remove_stopwords(text):
 
 def clean_white_space(text):
     return re.sub(' +', ' ', text)
+
+
+def check_spelling(input_text_or_list: Union[str, List[str]], lang='en'):
+    """ Check and correct spellings of the text list """
+    if input_text_or_list is None or len(input_text_or_list) == 0:
+        return ''
+    spelling_checker = SpellChecker(language=lang, distance=1)
+    # TODO: add acronyms into spell checker to ignore auto correction specified by _IGNORE_SPELLCHECK_WORD_FILE_PATH
+
+    if isinstance(input_text_or_list, str):
+        if not input_text_or_list.islower():
+            input_text_or_list = input_text_or_list.lower()
+        tokens = word_tokenize(input_text_or_list)
+    else:
+        tokens = [token.lower() for token in input_text_or_list if token is not None and len(token) > 0]
+    misspelled = spelling_checker.unknown(tokens)
+    for word in misspelled:
+        tokens[tokens.index(word)] = spelling_checker.correction(word)
+    return ' '.join(tokens).strip()
