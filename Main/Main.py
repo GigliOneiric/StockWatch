a5877af2cf5840984e7163a2d5272659506bda33
tstelter <tobias.stelter@outlook.com>
2022-07-07 18:25:54 +0200

    cleanup

--- Main/Main.py
+++ Main/Main.py
@@ -2,7 +2,7 @@
 from TextPreprocessing import TextPreprocess
 from TextPreprocessing.Helpers.StopWords import StopWords
 
-class Main:
-    s = StopWords(text).remove_stopwords()
-    print(s)
 
+class Main:
+    text = 'Test :)'
+    print(TextPreprocess.preprocess(text))
--- TextPreprocessing/TextPreprocess.py
+++ TextPreprocessing/TextPreprocess.py
@@ -1,18 +1,14 @@
 import re
-import TextPreprocessing.Helpers.Smileys
-import emoji
-import nltk
+import Config.text
 import contractions
+import nltk
 
-from TextPreprocessing.Helpers import Smileys
-from TextPreprocessing.Helpers.StopWords import StopWords
-from string import punctuation
-
-from nltk import word_tokenize
 from nltk.stem import SnowballStemmer
 from nltk.stem import WordNetLemmatizer
-
-import Config.text
+from string import punctuation
+from TextPreprocessing.Helpers import Smileys
+from TextPreprocessing.Helpers import Emojis
+from TextPreprocessing.Helpers.StopWords import StopWords
 
 
 def preprocess(text):
@@ -114,47 +110,11 @@ def replace_smiley(text):
 
 
 def remove_emojis(text):
-    emojis = re.compile("["
-                        u"\U0001F600-\U0001F64F"  # emoticons
-                        u"\U0001F300-\U0001F5FF"  # symbols & pictographs
-                        u"\U0001F680-\U0001F6FF"  # transport & map symbols
-                        u"\U0001F1E0-\U0001F1FF"  # flags (iOS)
-                        u"\U00002500-\U00002BEF"  # chinese char
-                        u"\U00002702-\U000027B0"
-                        u"\U00002702-\U000027B0"
-                        u"\U000024C2-\U0001F251"
-                        u"\U0001f926-\U0001f937"
-                        u"\U00010000-\U0010ffff"
-                        u"\u2640-\u2642"
-                        u"\u2600-\u2B55"
-                        u"\u200d"
-                        u"\u23cf"
-                        u"\u23e9"
-                        u"\u231a"
-                        u"\ufe0f"  # dingbats
-                        u"\u3030"
-                        "]+", re.UNICODE)
-    return re.sub(emojis, '', text)
+    return Emojis.remove_emojis(text)
 
 
 def replace_emojis(text):
-    # Group by same meaning
-    text = re.sub('[\U0001F550-\U0001F567]', " of the clock ", text)
-
-    # Replace emojis by their short text
-    text = emoji.demojize(text)
-
-    # Remove everything between emoji
-    text = re.sub(
-        r"(?<=:[a-zA-Z])(.*?)(?=:)",
-        lambda g: "{}".format(re.sub(r"[^a-zA-Z]", "", g.group(1))),
-        text,
-    )
-
-    # Remove : at the beginning and the end of an emoji
-    text = text.replace(":", " ")
-
-    return text
+    return Emojis.replace_emojis(text)
 
 
 def remove_stopwords(text):
