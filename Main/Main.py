85cbc5d100b5eb33e7bad9e6809bf4f19d04af7a
tstelter <tobias.stelter@outlook.com>
2022-11-07 17:30:59 +0100

    added kfp-support

--- Main/Main.py
+++ Main/Main.py
@@ -1,11 +1,8 @@
 from Twitter.TweetStreamListener import TweetStreamListener
 from ELK import DashBoardService
 from Stocks.StockStreamListener import listen_stream
-from Sentiment import sentimentAnalyzer_kfp
+
 
 class Main:
-
-    sentiment_analyzer = 'TextBlob' # TextBlob or KFP
-
     DashBoardService.load_dashboard()
     listen_stream()
